99d5d67d0662990b1a1b16d932b8c99fcce9feed
Luis David Aguilar <aguilar.luisdavid0@gmail.com>
2018-06-03 17:29:24 -0500

    Agregar el menu
    Se agrega el menu pero sin opciones por que no recuerdo cuales son

---
+++ menu.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+	using namessapce std;
+
+	int main(int argc, char const *argv[])
+	{
+		cout<<"\t Nombre del programa"<<endl;
+		cout<<"\t 1) "<<endl;
+		cout<<"\t 2) "<<endl;
+		cout<<"\t 3) "<<endl;
+		cout<<"\t 4) "<<endl;
+		cout<<"\t 5) "<<endl;
+		cout<<"\t 6) Salir"<<endl;
+		cout<<"\t Seleccione una opccion:  "<<endl;
+		cin>>op;
+		switch(op){
+			case 1: 
+				break;
+			case 2: 
+				break;
+			case 3: 
+				break;
+			case 4: 
+				break;
+			case 5: 
+				break;
+			case 6: 
+				break;
+			default: 
+				cout<<"\t Error opcion no valida"<<endl;
+				break;
+		}
+		return 0;
+	}
